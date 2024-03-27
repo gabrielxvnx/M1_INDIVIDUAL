aec9cbae9b7193bdb8d0d5cec5887c811ee60e4b
Gabriel Vinicius <112134365+gabrielxvnx@users.noreply.github.com>
2024-03-26 21:05:19 -0300

    Encapsulamento do processo de seleção
    Encapsulamento do código de seleção de candidatos em uma função.

--- main.py
+++ main.py
@@ -13,12 +13,18 @@ nota_minima_t = int(input("Digite a nota minima em Teste Teórico:"))
 nota_minima_p = int(input("Digite a nota minima em Teste Prático:"))
 nota_minima_s = int(input("Digite a nota minima em Avaliação Soft Skills:"))
 
-for candidato in candidatos:
-    nome, notas = candidato
-    nota_e, nota_t, nota_p, nota_s = [int(nota[1:]) for nota in notas.split('_')]
+def selecionar_candidatos(candidatos, nota_minima_e, nota_minima_t, nota_minima_p, nota_minima_s):
     
-    if nota_e >= nota_minima_e and nota_t >= nota_minima_t and nota_p >= nota_minima_p and nota_s >= nota_minima_s:
-        print(f"Nome: {nome} Notas: {nota_e}, {nota_t}, {nota_p}, {nota_s}")
-        selecionados.append(candidato)
-if len(selecionados) == 0:
-    print("Nenhum candidato atende aos criterios")
+    for candidato in candidatos:
+        nome, notas = candidato
+        nota_e, nota_t, nota_p, nota_s = [int(nota[1:]) for nota in notas.split('_')]
+        
+        if nota_e >= nota_minima_e and nota_t >= nota_minima_t and nota_p >= nota_minima_p and nota_s >= nota_minima_s:
+            print(f"Nome: {nome}, Notas: {nota_e}, {nota_t}, {nota_p} e {nota_s}")
+            selecionados.append(candidato)
+    
+    if len(selecionados) == 0:
+        print("Nenhum candidato atende aos criterios")
+
+print(selecionar_candidatos(candidatos, nota_minima_e, nota_minima_t, nota_minima_p, nota_minima_s))
+
