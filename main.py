f3fe8b4c1da5ec240d6caebafcdb180aa02680e6
Gabriel Vinicius <112134365+gabrielxvnx@users.noreply.github.com>
2024-03-27 15:29:57 -0300

    Update main.py
    Modifiquei a função selecionar_candidatos para retornar a lista de candidatos selecionados em vez de imprimir diretamente dentro da função.

--- main.py
+++ main.py
@@ -6,25 +6,26 @@ candidatos = [
     ("Marcos", "e10_t10_p8_s9")
 ]
 
-selecionados = []
-
-nota_minima_e = int(input("Digite a nota minima em Entrevista:"))
-nota_minima_t = int(input("Digite a nota minima em Teste Teórico:"))
-nota_minima_p = int(input("Digite a nota minima em Teste Prático:"))
-nota_minima_s = int(input("Digite a nota minima em Avaliação Soft Skills:"))
-
 def selecionar_candidatos(candidatos, nota_minima_e, nota_minima_t, nota_minima_p, nota_minima_s):
-    
+    selecionados = []
     for candidato in candidatos:
         nome, notas = candidato
         nota_e, nota_t, nota_p, nota_s = [int(nota[1:]) for nota in notas.split('_')]
         
         if nota_e >= nota_minima_e and nota_t >= nota_minima_t and nota_p >= nota_minima_p and nota_s >= nota_minima_s:
-            print(f"Nome: {nome}, Notas: {nota_e}, {nota_t}, {nota_p} e {nota_s}")
-            selecionados.append(candidato)
+            selecionados.append((nome, nota_e, nota_t, nota_p, nota_s))
     
-    if len(selecionados) == 0:
-        print("Nenhum candidato atende aos criterios")
+    return selecionados
 
-print(selecionar_candidatos(candidatos, nota_minima_e, nota_minima_t, nota_minima_p, nota_minima_s))
+nota_minima_e = int(input("Digite a nota minima em Entrevista: "))
+nota_minima_t = int(input("Digite a nota minima em Teste Teórico: "))
+nota_minima_p = int(input("Digite a nota minima em Teste Prático: "))
+nota_minima_s = int(input("Digite a nota minima em Avaliação Soft Skills: "))
 
+selecionados = selecionar_candidatos(candidatos, nota_minima_e, nota_minima_t, nota_minima_p, nota_minima_s)
+
+if selecionados:
+    for candidato in selecionados:
+        print(f"Nome: {candidato[0]}, Notas: {candidato[1]}, {candidato[2]}, {candidato[3]} e {candidato[4]}")
+else:
+    print("Nenhum candidato atende aos critérios")
